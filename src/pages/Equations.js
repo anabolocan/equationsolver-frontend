525307bf401a6cdb6c00bac14e9bcb73569afa1a
anabolocan <rihannaledi@mail.ru>
2021-05-25 18:33:52 +0300

    Using components on pages

---
+++ src/pages/Equations.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EquationSolverComponent from "../components/EquationSolverComponent";
+
+function Equations() {
+  return (
+    <div className='equations'>
+      <EquationSolverComponent/>
+    </div>
+  );
+}
+
+export default Equations;
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import EquationsComponent from "../components/EquationsComponent";
+
+function Home() {
+  return (
+    <div className='home'>
+        <EquationsComponent/>
+    </div>
+  );
+}
+
+export default Home;
